470963678e99f2b4713c7e451a78d5bf7482a5d2
Binjam Gebre <info@react-entwickler.de>
2018-08-27 09:32:24 +0200

    Alles angelegt, fertig für den Start

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Startseite from './Startseite';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+
+
+<Startseite />,
+
+
+document.getElementById('root'));
+registerServiceWorker();
